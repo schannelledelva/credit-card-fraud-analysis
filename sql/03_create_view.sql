2d555aaae3eedf413f19cec6a8747dd9832142df
schannelledelva <schannelledelva@gmail.com>
2026-07-19 20:15:59 +0100

    Create analytical view for clean fraud data

--- sql/03_create_view.sql
+++ sql/03_create_view.sql
@@ -1,11 +1,11 @@
 -- Creates analytical view selecting only columns used in Power BI
 -- Pre-splits trans_date_trans_time into separate date and time columns
 -- Connect Power BI to this view, not the raw staging table
-
-CREATE VIEW view_name AS
-SELECT 
-    CAST(LEFT(trans_date_trans_time, 10) AS DATE)        AS Transaction_date,
-    CAST(RIGHT(trans_date_trans_time, 8) AS TIME)        AS Transaction_time,
+CREATE VIEW v_Clean_Fraud_data AS
+SELECT
+    CAST(TRY_CONVERT(datetime, trans_date_trans_time) AS date) AS Transaction_date,
+    CAST(TRY_CONVERT(datetime, trans_date_trans_time) AS time) AS Transaction_time,
+    cc_num,
     merchant,
     category,
     amt,
@@ -13,4 +13,4 @@ SELECT
     state,
     job,
     is_fraud
-FROM fraudTrain -- Or your custom table name
+FROM fraudTrain; -- Or your custom table name
